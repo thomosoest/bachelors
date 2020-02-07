9fbca7539e543155e4b0ae0ff7972f17771741de
Thomas Oestli <oestli@hotmail.no>
2020-02-07 13:51:49 +0100

    Joining a company

--- routes/companies.js
+++ routes/companies.js
@@ -33,4 +33,33 @@ try {
 
 })
 
+
+// @route       PUT api/companies/join/:id
+// @desc        Join a company
+// @access      Private
+
+router.put('/join/:id', auth, 
+    async (req,res) =>{
+
+        try {
+            const company = await Company.findById(req.params.id);
+
+            //Check if the user already exists in the company
+            if(company.ansatte.filter(employee => employee.user.toString() === req.user.id).length > 0) {
+                return res.status(400).json({ msg: 'Already Joined'});
+            }
+
+            company.ansatte.unshift({ user: req.user.id});
+
+            await company.save();
+
+            res.json(company.ansatte);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+
+});
+
+
 module.exports = router;
