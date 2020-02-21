71b66bef0fe92509bfc559ff8e546682ec456bea
Thomas Oestli <oestli@hotmail.no>
2020-02-21 12:44:55 +0100

    changed companies view for owner

--- client/src/components/companies/CompanyItem.js
+++ client/src/components/companies/CompanyItem.js
@@ -9,18 +9,29 @@ const CompanyItem = (props) => {
         joinCompany(props.company._id);
     }
 
-    let output = <button onClick={join} className="btn btn-dark btn-sm">JOIN</button>;
-    if (props.case === "owner") {
-        output = null;
-    } 
-    else if(props.case === "employee") {
-        output = <p>Employee view</p>;
+    const toDashboard = () => {
+        console.log("To Dashboard");
+    }
+
+
+
+    let output = <button onClick={join} className="btn btn-dark btn-sm">Bli Med</button>;
+    switch (props.case ) {
+        case "owner":
+                output = <button onClick={toDashboard} className="btn btn-dark btn-sm">Se Dashbord</button>;
+            break;
+        case "ansatt":
+            output = <p>Ansatt perspektiv</p>;        
+            break;
+    
+        default: output = <button onClick={join} className="btn btn-dark btn-sm">Bli Med</button>;
+            break;
     }
 
     return (
         <div className="card bg-light">
             <h3>{props.company.companyName}</h3>
-            <p>Owner {props.company.user.name}</p>
+            <p>Eier {props.company.user.name}</p>
             {output}
         </div>
     )
