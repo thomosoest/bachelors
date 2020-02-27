d6ab02a88e83b5777426ef84d7627fcce06b1f20
askelej <askeltechno@hotmail.com>
2020-02-27 13:29:02 +0100

    no

--- client/src/components/companies/Bank.js
+++ client/src/components/companies/Bank.js
@@ -6,12 +6,13 @@ import CompanyContext from '../../context/company/companyContext';
 const Bank = () => {
 
     const companyContext = useContext(CompanyContext);
-
+    
     useEffect(() => {
         companyContext.getBank();
         // eslint-disable-next-line
     }, []);
 
+    console.log(companyContext.bank);
     return (<div>
 
         <p>Bank1</p>
