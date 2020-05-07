0ad2a08441d59364cca5533c53d62fe5886a8247
askelej <askeltechno@hotmail.com>
2020-05-07 14:47:28 +0200

    Fixed class to className warning

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const DashBoardActions = () => {
 return (
     <div className='dash-buttons'>
-        <Link to='/edit-profile' class='btn btn-light'> <i class='fab fa-user-circle text-primary' /> Rediger Bio
+        <Link to='/edit-profile' className='btn btn-light'> <i className='fab fa-user-circle text-primary' /> Rediger Bio
         </Link>
     </div>
     )
