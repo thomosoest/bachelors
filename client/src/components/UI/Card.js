184bb5d0f89da36c16beab8acd225060715072e4
askelej <askeltechno@hotmail.com>
2020-05-07 15:23:18 +0200

    Made card better, added date to courseItem, fixed selected employee display

--- client/src/components/UI/Card.js
+++ client/src/components/UI/Card.js
@@ -15,7 +15,10 @@ const Card = ({title, text, click, buttonName, children, arg, date, status}) => 
         <p>{text}</p>
         <h6>{date}</h6>
         <h6>{status}</h6>
-        <button onClick={clickHandler} className="btn btn-dark btn-sm">{buttonName}</button>
+        {(buttonName || click)? 
+            (<button onClick={clickHandler} className="btn btn-dark btn-sm">{buttonName}</button>) 
+            : null}
+        
         {children}
         </div>
     )
